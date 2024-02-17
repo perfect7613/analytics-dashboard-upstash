e96a86be6fbe5195e530a63ef0704b4d1dda83ca
Amey Muke <amitjoshi2052003@gmail.com>
2024-02-18 01:13:29 +0530

    Done2

--- lib/redis.ts
+++ lib/redis.ts
@@ -2,5 +2,5 @@ import { Redis } from '@upstash/redis'
 
 export const redis = new Redis({
   url: 'https://eu2-holy-seal-32504.upstash.io',
-  token: process.env.NEXT_PUBLIC_REDIS_KEY!,
+  token: process.env.REDIS_KEY!,
 })
