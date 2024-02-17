e10da66d7362978bfa6bcfda753cdf9aaf1465d6
Amey Muke <amitjoshi2052003@gmail.com>
2024-02-17 23:54:49 +0530

    Hope so

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    rules: {
+        'react/jsx-key': 'off', // Not recommended
+      },
+};
 
 export default nextConfig;
